5eaa633b1e234208211c1244669dbf34fd12fe58
Haowen <haowen737@gmail.com>
2018-11-23 21:48:24 +0800

    Feat backup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { config } from 'dotenv'
+import useMSCred from './middleware/useMSCred'
 
 async function bootstrap() {
+  const creds = config()
+  await useMSCred()
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3002);
 }
-bootstrap();
+
+bootstrap()
